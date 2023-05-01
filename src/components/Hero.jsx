4cc791a5bfcedcdf54704a6cb53e133387c4bf88
Rishikesh K V <rishikeshkaroth@gmail.com>
2023-05-02 00:46:59 +0530

    Made initial web design

---
+++ src/components/Hero.jsx
@@ -0,0 +1,35 @@
+import { logo } from "../assets";
+
+const Hero = () => {
+  return (
+    <header
+      className="w-full flex justify-center
+    items-center flex-col"
+    >
+      <nav
+        className="flex justify-between
+        items-center w-full mb-10 pt-3"
+      >
+        <img src={logo} alt="sumz_logo" className="w-28 object-contain" />
+
+        <button
+          type="button"
+          onClick={() => window.open("")}
+          className="black_btn"
+        >
+          Github
+        </button>
+      </nav>
+      <h1 className="head_text">
+        Summarize Article with <br className="max-md:hidden" />
+        <span className="orange_gradient">OpenAI GPT-3</span>
+      </h1>
+      <h2 className="desc">
+        Simplify your reading with Summize, an open-source article summarizer
+        that transfomrs lengthy article into clear and concise summaries{" "}
+      </h2>
+    </header>
+  );
+};
+
+export default Hero;
